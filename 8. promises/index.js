add042b61327b8e9e051093567e609ed9c184933
habeeb321 <habeebrh321@gmail.com>
2025-09-07 17:33:28 +0530

    Learned promise

---
+++ 8. promises/index.js
@@ -0,0 +1,21 @@
+
+function delayedFn(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+console.log('starting promise');
+delayedFn(2000).then(() => console.log('after 2 seconds resolved'));
+console.log('end');
+
+
+function devideFn(num1, num2) {
+    return new Promise((resolve, reject) => {
+        if (num2 === 0) {
+            reject('can not perform division by 0');
+        } else {
+            resolve(num1 / num2);
+        }
+    });
+}
+
+devideFn(10, 5).then((result) => console.log(result, 'res')).catch((error) => console.log(error, 'error'));
